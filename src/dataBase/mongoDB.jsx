693c4433e9752875d8746c6d6809f35f02565fcf
Martin <tincho1597lopez@gmail.com>
2023-02-07 21:19:45 -0300

    Added functionality for connecting to MongoDB, searching for jobs by ID, finding all jobs, inserting new jobs and error handling in the code.

--- src/dataBase/mongoDB.jsx
+++ src/dataBase/mongoDB.jsx
@@ -1,45 +1,57 @@
 const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
-const username = process.env.MONGO_DB_USERNAME; // ARREGLAR!
-const password = process.env.MONGO_DB_PASSWORD; // ARREGLAR!
-const host = 'cluster0.ncc21bh.mongodb.net';
-const uri = `mongodb+srv://tincho1597:Martolomeo18@cluster0.ncc21bh.mongodb.net/?retryWrites=true&w=majority`;
+const { username, password, host, dbName, collectionName } = require('./j6h15.jsx');
+
+const uri = `mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority`;
 let client;
 let collection;
-//mongodb+srv://tincho1597:Martolomeo18@cluster0.ncc21bh.mongodb.net/?retryWrites=true&w=majority
-//mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority
 
+// Connects to the MongoDB database using the MongoClient object
 async function connectToDB() {
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    await client.connect();
-    collection = client.db('job_list').collection('jobs');
+    try {
+        client = new MongoClient(uri, { useNewUrlParser: true });
+        await client.connect();
+        collection = client.db(dbName).collection(collectionName);
+        console.log('Successfully connected to the database.');
+    } catch (error) {
+        console.error(`Error connecting to the database: ${error}`);
+    }
 }
 
+// Finds a job in the database based on its ID
 async function findJob(id) {
-    const result = await collection.findOne({ id });
-    console.log(result);
+    try {
+        const result = await collection.findOne({ id });
+        console.log(result);
+    } catch (error) {
+        console.error(`Error finding job: ${error}`);
+    }
 }
 
+// Closes the MongoDB database connection
 async function close() {
     client.close();
 }
+
+// Finds all jobs in the database and logs the results
 async function findAllJobs() {
-    const cursor = collection.find({});
-    const jobs = await cursor.toArray();
-    console.log(jobs);
+    try {
+        const cursor = collection.find({});
+        const jobs = await cursor.toArray();
+        console.log(jobs);
+    } catch (error) {
+        console.error(`Error finding all jobs: ${error}`);
+    }
 }
 
+// Inserts a new job into the database
 async function insertNewJob(job) {
-    const result = await collection.insertOne(job);
-    console.log(`Inserted job with ID: ${result.insertedId}`);
+    try {
+        const result = await collection.insertOne(job);
+        console.log(`Inserted job with ID: ${result.insertedId}`);
+    } catch (error) {
+        console.error(`Error inserting new job: ${error}`);
+    }
 }
-
-
-/* async function findAllJobsAndSaveToFile() {
-    const cursor = collection.find({});
-    const jobs = await cursor.toArray();
-    fs.writeFileSync('jobs.json', JSON.stringify(jobs), 'utf-8');
-} */
 
 
 module.exports = {
